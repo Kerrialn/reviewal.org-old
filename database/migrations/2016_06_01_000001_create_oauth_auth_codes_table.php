7e2fbda935254daea4b064aad021acfa521eafa9
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2021-05-25 10:03:37 +0200

    update migrations

--- database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
+++ database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
@@ -31,7 +31,7 @@ class CreateOauthAuthCodesTable extends Migration
     public function up()
     {
         $this->schema->create('oauth_auth_codes', function (Blueprint $table) {
-            $table->id();
+            $table->string('id', 100)->primary();
             $table->unsignedBigInteger('user_id')->index();
             $table->uuid('client_id');
             $table->text('scopes')->nullable();
