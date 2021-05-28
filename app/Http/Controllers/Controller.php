66b734815d81ac575f9a56f42149718e14a5977d
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2021-05-28 16:54:09 +0200

    update create

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -25,45 +25,47 @@ class Controller extends BaseController
      */
     public function create(Request $request)
     {
-        $user = Auth::user();
-        $address = Address::firstOrNew([
-            'premise'  => $request->address['premise'],
-            'floor'  => $request->address['floor'],
-            'line_one'  => strtolower($request->address['line_one']),
-            'city'  => strtolower($request->address['city']),
-            'postal_code'  => strtolower($request->address['postal_code']),
-            'country_code' => strtolower($request->address['country_code']),
-        ]);
+        if (Auth::check()) {
+            $user = Auth::user();
+            $address = Address::firstOrNew([
+                'premise'  => $request->address['premise'],
+                'floor'  => $request->address['floor'],
+                'line_one'  => strtolower($request->address['line_one']),
+                'city'  => strtolower($request->address['city']),
+                'postal_code'  => strtolower($request->address['postal_code']),
+                'country_code' => strtolower($request->address['country_code']),
+            ]);
 
-        $address->id = Str::uuid();
-        $address->save();
+            $address->id = Str::uuid();
+            $address->save();
 
-        $hasUserReviewedBefore = Address::find($address->id)->with(['reviews' => function ($query) use ($user) {
-            return $query->where('user_id', '=>', $user->id);
-        }])->exists();
+            $hasUserReviewedBefore = Address::find($address->id)->with(['reviews' => function ($query) use ($user) {
+                return $query->where('user_id', '=>', $user->id);
+            }])->exists();
 
-        if ($hasUserReviewedBefore) {
-            abort(400, 'address already reviewed');
+            if ($hasUserReviewedBefore) {
+                abort(400, 'address already reviewed');
+            }
+
+            $review = Review::create([
+                'title' => $request->review['title'],
+                'summary' => $request->review["summary"],
+                'address_id' => $address->id,
+                'user_id' => $user->id
+            ]);
+            $review->save();
+
+            $rating = Rating::create([
+                "landlord" =>  $request->rating["landlord"],
+                "premise" =>  $request->rating["premise"],
+                "area" =>  $request->rating["area"],
+                "transport" =>  $request->rating["transport"],
+                "noise" =>  $request->rating["noise"],
+                "overall" => ($request->rating["landlord"] + $request->rating["premise"] + $request->rating["area"] + $request->rating["transport"] + $request->rating["noise"]) / 5,
+                'review_id' => $review->id
+            ]);
+            $rating->save();
         }
-
-        $review = Review::create([
-            'title' => $request->review['title'],
-            'summary' => $request->review["summary"],
-            'address_id' => $address->id,
-            'user_id' => $user->id
-        ]);
-        $review->save();
-
-        $rating = Rating::create([
-            "landlord" =>  $request->rating["landlord"],
-            "premise" =>  $request->rating["premise"],
-            "area" =>  $request->rating["area"],
-            "transport" =>  $request->rating["transport"],
-            "noise" =>  $request->rating["noise"],
-            "overall" => ($request->rating["landlord"] + $request->rating["premise"] + $request->rating["area"] + $request->rating["transport"] + $request->rating["noise"]) / 5,
-            'review_id' => $review->id
-        ]);
-        $rating->save();
     }
 
 
